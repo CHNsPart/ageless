0c65804393d74d73f6d39b9af98cd63388ad57ab
chnspart <imchn24@gmail.com>
2024-06-05 21:24:28 -0400

    Homepage completed II

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 export default function Footer() {
   return (
-    <footer className="text-gray-600 body-font">
-      <div className="container px-5 py-24 mx-auto flex md:items-center lg:items-start md:flex-row md:flex-nowrap flex-wrap flex-col">
+    <footer className="text-gray-600 body-font w-full md:px-12 lg:px-48">
+      <div className="px-5 py-24 mx-auto flex md:items-center lg:items-start md:flex-row md:flex-nowrap flex-wrap flex-col">
         <div className="w-64 flex-shrink-0 md:mx-0 mx-auto text-center md:text-left">
           <a className="flex title-font font-medium items-center md:justify-start justify-center text-gray-900">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full" viewBox="0 0 24 24">
@@ -35,8 +35,8 @@ export default function Footer() {
           ))}
         </div>
       </div>
-      <div className="bg-gray-100">
-        <div className="container mx-auto py-4 px-5 flex flex-wrap flex-col sm:flex-row">
+      <div>
+        <div className="mx-auto py-4 px-5 flex flex-wrap flex-col sm:flex-row">
           <p className="text-gray-500 text-sm text-center sm:text-left">© 2024 Ageless —
             <a href="https://oonkoo.com/" rel="noopener noreferrer" className="text-primary ml-1" target="_blank">OonkoO</a>
           </p>
